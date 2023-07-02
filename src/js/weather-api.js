025de8d5963f042a01342e9f06c78c70ffd2e162
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-07-02 15:11:31 +0300

    weather

---
+++ src/js/weather-api.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "9c6d356df795525b572ef209f6cb500f";
+
+const getWeatherByCityName = (cityName) =>
+  fetch(`${BASE_URL}?q=${cityName}&appid=${API_KEY}&lang=ua&units=metric`).then(
+    (res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      return res.json();
+    }
+  );
+
+export default getWeatherByCityName;
